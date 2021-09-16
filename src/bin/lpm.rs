6f21172fcdc49c85ab0d79d79ab4366a7a1fcb05
Xophe <christophe.dousson@orange.com>
2021-09-16 16:45:55 +0200

    warning messages are redirected to stderr

--- src/bin/lpm.rs
+++ src/bin/lpm.rs
@@ -4,6 +4,8 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
 use std::ptr::null_mut;
+use std::fs::File;
+use  std::os::unix::io::AsRawFd;
 
 static EMPTY_PREFIX: &str = "<empty prefix>";
 
@@ -11,53 +13,15 @@ type Ipv4Prefix = IpWholePrefix<Ipv4>;
 type Ipv6Prefix = IpWholePrefix<Ipv6>;
 
 fn main() {
-    let file = env::args().skip(1).next().expect("needs a LPM file");
-
-    let fd = unsafe {
-        let file = CString::new(file.clone()).unwrap();
-        libc::open(file.as_c_str().as_ptr(), libc::O_RDONLY)
-    };
-    if fd == -1 {
-        panic!("FATAL: can't open LPM file {}", file);
-    }
-    let length = {
-        let mut sb= libc::stat {
-            st_dev: 0,
-            st_mode: 0,
-            st_nlink: 0,
-            st_ino: 0,
-            st_uid: 0,
-            st_gid: 0,
-            st_rdev: 0,
-            st_atime: 0,
-            st_atime_nsec: 0,
-            st_mtime: 0,
-            st_mtime_nsec: 0,
-            st_ctime: 0,
-            st_ctime_nsec: 0,
-            st_birthtime: 0,
-            st_birthtime_nsec: 0,
-            st_size: 0,
-            st_blocks: 0,
-            st_blksize: 0,
-            st_flags: 0,
-            st_gen: 0,
-            st_lspare: 0,
-            st_qspare: [0;2]
-        };
-        unsafe {
-            if libc::fstat(fd, &mut sb as * mut libc::stat) == -1 {
-                panic!("FATAL: can't access to LPM file {}", file);
-            }
-        }
-        sb.st_size as usize
-    };
+    let filename = env::args().skip(1).next().expect("needs a LPM file");
+    let file = File::open(filename).expect("can’t open LPM file");
+    let length = file.metadata().unwrap().len() as usize;
 
     let lpmfile = unsafe {
-        libc::mmap(null_mut(), length, libc::PROT_READ, libc::MAP_PRIVATE, fd, 0) as *const c_char
+        libc::mmap(null_mut(), length, libc::PROT_READ, libc::MAP_PRIVATE, file.as_raw_fd(), 0) as *const c_char
     };
     if lpmfile.is_null() {
-        panic!("FATAL: can't load LPM file {}", file);
+        panic!("FATAL: can't load LPM file");
     }
     let lpmfile = unsafe { std::str::from_utf8(CStr::from_ptr(lpmfile).to_bytes()) }.unwrap();
 
@@ -75,14 +39,14 @@ fn main() {
             } else if let Ok(pfx) = prefix.parse::<Ipv6Prefix>() {
                 map6.insert(pfx, line);
             } else {
-                println!("WARN: skip bad formatted line: {}", line);
+                eprintln!("WARN: skip bad formatted line: {}", line);
             }
         });
     //map6.generate_graphviz_file("ipv6".into());
     //maq’p6.open_dot_view();
     let map4 = map4.compile();
     let map6 = map6.compile();
-    println!("ready ....");
+
     loop {
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
@@ -95,7 +59,7 @@ fn main() {
                 } else if let Ok(addr) = input.parse::<Ipv6Addr>() {
                     println!("{}", map6.lookup(&addr).1);
                 } else {
-                    println!("WARN: can’t parse '{}' (not an IP address)", input);
+                    eprintln!("WARN: can’t parse '{}' (not an IP address)", input);
                 }
             }
             Err(error) => panic!("{}", error)
